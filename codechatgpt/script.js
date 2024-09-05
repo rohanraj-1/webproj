86f5c026fbc02ccdff637337ed97296f409ff8b1
rohanraj-1 <deshmukhrohan034@gmail.com>
2024-09-05 20:34:08 +0530

    first commit

---
+++ codechatgpt/script.js
@@ -0,0 +1,12 @@
+const carousel = new bootstrap.Carousel('#myCarousel')
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+const myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  // do something...
+})
